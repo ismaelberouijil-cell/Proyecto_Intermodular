d6e2fc09ba4ed47840d5bb7da5a583936db99db9
dcouto1008 <coutodiego53@gmail.com>
2026-04-20 14:49:26 +0200

    Tercer comit

---
+++ Java/Cliente.java
@@ -0,0 +1,10 @@
+
+public class Cliente {
+
+    private String nombre;
+    private String  Apellido;
+    private String prApellido;
+    private String dni;
+    private String sgapellido;
+    
+}
