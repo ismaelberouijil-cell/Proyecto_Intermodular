e6c619049dedada6222243c0ec71ea9b114d1775
dcouto1008 <coutodiego53@gmail.com>
2026-04-21 10:51:26 +0200

    Cuarto commit

--- Base de Datos/Catalogo.sql
+++ Base de Datos/Catalogo.sql
@@ -1,13 +1,13 @@
-Create Databases CatalogoMobile
+Create eSINTERMINAR;
 
-Use CatalogoMobile
+Use eSINTERMINAR;
 
 Create table Marca(
     ID_Marca varchar(50) not null,
     Primary key (ID_Marca),
     nombre varchar(20) not null,
     Pais_Origen varchar(20) not null
-)
+);
 Create table Movil(
     ID_Movil varchar(50) not null,
     Primary key (ID_Movil),
@@ -28,12 +28,39 @@ Create table Cliente(
     SgApellido varchar(30) not null,
     telefono varchar (15) not null,
     email varchar(30) not null,
+    Contrasena varchar(10) not null,
     direccion varchar(30) not null
+    
 );
 
 Create table Envio(
     ID_ENVIO varchar(50) not null,
     primary key(ID_ENVIO),
-    
-)
+    Empresa_Envio Date,
+    Estado_Envio varchar(10) not null
+    Fecha_Envio Date,
+    ID_Pedido varchar(30) not null,
+    foreign key (ID_Pedido) references Cliente(ID_Pedido)
+);
 
+Create table Pago(
+    ID_Pago varchar(30) not null,
+    ID_Pedido varchar(30) not null,
+    Metodo_Pago varchar(10) not null,
+    Fecha_Pago Date,
+    Importe decimal(10,2)
+    foreign key (ID_Pedido) references Cliente(ID_Pedido)
+);
+
+Create table IBAN(
+    
+    codIban int Primary Key,
+    CodPais varchar(2) not null,
+    DC varchar(2) not null,
+    Entidad varchar(4) not null,
+    cSucursal varchar(4) not null,
+    cDCCta varchar(10)not null,
+    cnumcuenta varchar(20) not null,
+    dni varchar(20) not null,
+    Foreign Key(dni) references Cliente(dni)
+);
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 #Mi_Proyecto
 
 #Creacion de cesta
+
+#Cambio_Drastico
+##Hemos decidido quitar la cesta porque nos va a liar, tambien hemso pensado en que los clientes puedan crear usuarios, puedan registrarse e iniciar sesion, por ahora solo tenemos en el html para que inicie
